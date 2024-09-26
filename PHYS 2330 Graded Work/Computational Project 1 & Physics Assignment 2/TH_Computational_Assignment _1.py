60939a5e42825b403a68f421bcb5b5f14095a227
TevHeath <tevinheath.beng@gmail.com>
2024-09-26 13:30:55 -0400

    Created the Electrical Potential Function

--- PHYS 2330 Graded Work/Computational Project 1 & Physics Assignment 2/TH_Computational_Assignment _1.py
+++ PHYS 2330 Graded Work/Computational Project 1 & Physics Assignment 2/TH_Computational_Assignment _1.py
@@ -9,3 +9,33 @@
     #These components are the following, position, charge, density, potential contour, and directional field
 #Fourth/Finally, Save your plot(s) as a single image file, “fieldPlot.png”. And name it accordingly.
 #Part B requires that I attempt to locate the area that the charges are placed and their charge
+
+
+"""
+PART A
+"""
+
+""" Section 1:  Start by importing relevant libraries
+#---------------------------------------------------------------------------"""
+import numpy as np
+# import matplotlib.pyplot as plt
+
+""" Section 2:  Define functions for the script
+#---------------------------------------------------------------------------"""
+
+# Return the electric potential, V, due to a charge "q" located at position
+# rq = (xq,yq).  Evaluate V at the given position (x,y)
+
+def e_potential(q, rq, x, y):
+    k = 8.99e9 #Colombs Constant
+    r = np.sqrt((rq[0] - x) ** 2 + (rq[1] - y) **2) #Distance between the position
+    V = k * q / r #Note electrical potential has been written as U also
+    return V #This return allows for the output to be calculated
+
+#print(V) #I will need values to provide in order to determine the necessary values
+
+
+
+""" 
+PART B
+"""
