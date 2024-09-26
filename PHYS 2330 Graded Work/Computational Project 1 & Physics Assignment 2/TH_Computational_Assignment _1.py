a92b7525b4631c10129f16f5c4941a1898ccceea
TevHeath <tevinheath.beng@gmail.com>
2024-09-26 12:36:33 -0400

    Added new file for Assignment

---
+++ PHYS 2330 Graded Work/Computational Project 1 & Physics Assignment 2/TH_Computational_Assignment _1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Computational Exercise 1 for PHYS*2330 - Fall 2024
+# ------------------------------------------------------------------------------
+## For this assignment, I need two utilize two functions, one given and the other created
+# First, a potential function that was created during tutorial
+# Second, design electric field function utilizing the Electric Field equation from Class
+# Third I will need to plot both of these function on a plot by highlight key components
+    #These components are the following, position, charge, density, potential contour, and directional field
+#Fourth/Finally, Save your plot(s) as a single image file, “fieldPlot.png”. And name it accordingly.
+#Part B requires that I attempt to locate the area that the charges are placed and their charge
