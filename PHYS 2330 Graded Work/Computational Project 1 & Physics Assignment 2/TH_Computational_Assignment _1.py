4847a56e2a6290106a08eebbff4763c45a3dd3a1
TevHeath <tevinheath.beng@gmail.com>
2024-09-26 21:17:17 -0400

    Improved image change with correct position, charges with contour lines

--- PHYS 2330 Graded Work/Computational Project 1 & Physics Assignment 2/TH_Computational_Assignment _1.py
+++ PHYS 2330 Graded Work/Computational Project 1 & Physics Assignment 2/TH_Computational_Assignment _1.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # Computational Exercise 1 for PHYS*2330 - Fall 2024
 # ------------------------------------------------------------------------------
 ## For this assignment, I need two utilize two functions, one given and the other created
@@ -14,7 +12,6 @@
 """
 PART A
 """
-from IPython.core.pylabtools import figsize
 
 """ Section 1:  Start by importing relevant libraries
 #---------------------------------------------------------------------------"""
@@ -25,116 +22,116 @@ from scipy.constants import epsilon_0
 """ Section 2:  Define functions for the script
 #---------------------------------------------------------------------------"""
 
-# Return the electric potential, V, due to a charge "q" located at position
-# rq = (xq,yq).  Evaluate V at the given position (x,y)
-
-def electric_potential(q, rq, x, y):
-    #k = 8.99e9  # Coulombs Constant - I could also use another
+def electric_potential(q, rq, X, Y):
     e_o = epsilon_0
-    Fr_pi = (4*np.pi)
+    Fr_pi = (4 * np.pi)
     K = 1 / (Fr_pi * e_o)
-    r = np.sqrt((rq[0] - x) ** 2 + (rq[1] - y) **2) #Distance between the position
-    V = K * q / r #Note electrical potential has been written as U also
-    return V #This return allows for the output to be calculated
+    r = np.sqrt((rq[0] - X) ** 2 + (rq[1] - Y) ** 2)  # Distance between the position
+    V = K * q / r
+    return V
 
-#print(V) #I will need values to provide in order to determine the necessary values
-
-
-
-def electric_field(q, rq, x,y):
+def electric_field(q, rq, X, Y):
     e_o = epsilon_0
-    Fr_pi = (4*np.pi)
+    Fr_pi = (4 * np.pi)
     K = 1 / (Fr_pi * e_o)
 
-    #Calculation of the distance between charge and the observation point
-    r = np.sqrt((x - rq[0]) ** 2 + (y - rq[1]) ** 2)
+    # Distance between charge and observation points
+    r = np.sqrt((X - rq[0]) ** 2 + (Y - rq[1]) ** 2)
 
-    #Calculation of the angle
-    theta = np.arctan(y - rq[1], x -rq[0])
+    # Angle calculation
+    theta = np.arctan2((Y - rq[1]), (X - rq[0]))
 
-    #Electric field components using r-squared and theta
-    Efield_comp_x = K * q / (r ** 2) * np.cos(theta) #np.cos represents x direction
-    Efield_comp_y = K * q / (r ** 2) * np.sin(theta) #np.cos represents x direction
+    # Electric field components using r-squared and theta
+    Efield_comp_x = K * q / (r ** 2) * np.cos(theta)  # x direction
+    Efield_comp_y = K * q / (r ** 2) * np.sin(theta)  # y direction
 
     return Efield_comp_x, Efield_comp_y
-
-#Now that all the function are created
-#We can verify using print command and testing
 
 """ Section 3:  Main body of code 
 #---------------------------------------------------------------------------"""
 # Create a 2D grid of x, y points using numpy meshgrid function
 
 nx, ny = 100, 100
-x = np.linspace(-5,5,nx)
-y = np.linspace(-5,5,ny)
-X,Y = np.meshgrid(x,y)
+x = np.linspace(-5, 5, nx)
+y = np.linspace(-5, 5, ny)
+X, Y = np.meshgrid(x, y)
 
-#Charges
-
+# Charges
 q1 = -3
 q2 = -1
-q3 = +2         #the plus + is not needed, but useful for clarity
+q3 = +2
 q4 = +6
 
-#Positions (after the comment)
-'''
-Note the following:
-1. Position of the charges on the vertices of a square of side length d = 3m
-2. Center the square position is at x = -1 and y =0.5
-*This is import to keep in mind especially as it relates understanding Part B
-*I will need to complete my hand calculation to determine these locations
-'''
+# Positions
+x1, y1 = 1 / np.sqrt(2), 3 / np.sqrt(2)
+x2, y2 = -1 / np.sqrt(2), 3 / np.sqrt(2)
+x3, y3 = -1, 3.5
+x4, y4 = 1 / np.sqrt(2), 0.5 - 3 / np.sqrt(2)
 
-#Charge Position #1: -3
-x1 = 1/(np.sqrt(2))
-y1 = 3/(np.sqrt(2))
+# Electric Potential Calculation
+V_potential = electric_potential(q1, [x1, y1], X, Y)
+V_potential += electric_potential(q2, [x2, y2], X, Y)
+V_potential += electric_potential(q3, [x3, y3], X, Y)
+V_potential += electric_potential(q4, [x4, y4], X, Y)
 
-#Charge Position #1: -1
-x2 = -1/(np.sqrt(2))
-y2 = 3/(np.sqrt(2))
+# Electric Field Calculation (using 2D grid X and Y)
+EField_x1, EField_y1 = electric_field(q1, [x1, y1], X, Y)
+EField_x2, EField_y2 = electric_field(q2, [x2, y2], X, Y)
+EField_x3, EField_y3 = electric_field(q3, [x3, y3], X, Y)
+EField_x4, EField_y4 = electric_field(q4, [x4, y4], X, Y)
 
-#Charge Position #1: +2
-x3 =-1
-y3 = 3.5
+# Sum the electric field components
+Ex = EField_x1 + EField_x2 + EField_x3 + EField_x4
+Ey = EField_y1 + EField_y2 + EField_y3 + EField_y4
 
-#Charge Position #1: +6
-x4 = 1/(np.sqrt(2))
-y4 = 0.5 + 3
-
-Vfunzies = electric_potential(q1, [x1,y1], X,Y)
-Vfunzies += electric_potential(q2, [x2,y2], X,Y)
-Vfunzies += electric_potential(q3, [x3,y3], X,Y)
-Vfunzies += electric_potential(q4, [x4,y4], X,Y)
-
-#Note I haven't added the electrical field component yet.
-#This is because I would like to review the position currently
-
-""" Section 4:  Plot your results as a contour plot (re-created from Tutorial, Dr. Massa)
+""" Section 4:  Plot your results as a contour plot
 #---------------------------------------------------------------------------"""
-fig = plt.figure(figsize(20,10))
+fig = plt.figure(figsize=(21, 7))
 ax1 = fig.add_subplot(121)
 ax2 = fig.add_subplot(122)
 
-ax1.set_title("X meshgrid", fontsize = 20) #Adds title to the X axis
-ax2.set_title("Y meshgrid", fontsize = 20) #Adds title to the Y axis
+ax1.set_title("Electric Potential", fontsize=20)
+ax2.set_title("Electric Field (Y meshgrid)", fontsize=20)
 
-im1 = ax1.imshow(Vfunzies, cmap = 'coolwarm', extent = (-6, 6, 6, 6), origin = "lower")
+# Electric potential plot
+im1 = ax1.imshow(V_potential, cmap='coolwarm', extent=(-6, 6, -5, 5), origin="lower")
+plt.colorbar(im1, ax=ax1)
+
+# Y meshgrid plot
 im2 = ax2.imshow(Y, cmap='coolwarm')
-plt.colorbar(im2)
+plt.colorbar(im2, ax=ax2)
 
-# Contour plots set equipotential lines that are equally spaced in values of V
-# We can specify the values of the contour lines
-
-VpLines = 10 ** np.linspace(6,12,5)
+# Contour plot for electric potential
+VpLines = 10 ** np.linspace(6, 12, 5)
 VpLines = sorted(list(-VpLines) + list(VpLines))
+ax1.contour(X, Y, V_potential, colors='r', linewidths=0.5, levels=np.linspace(np.min(V_potential), np.max(V_potential), 50))
 
-ax1.contour(X,Y,Vfunzies, color='r', linewidths=1, levels=VpLines)
-#Notice that the Vplines within the contours is connected to the line 129
-#To make adjustments change within 129
+""" Section 5: Vector plot (Electric Field)
+#---------------------------------------------------------------------------"""
+fig2 = plt.figure(figsize=(21, 7))
+ax3 = fig2.add_subplot(131)
+# Streamplot for the electric field (vX, vY as Ex, Ey)
+ax3.streamplot(X, Y, Ex, Ey, color='blue', cmap='coolwarm', density=1.0)
 
-# Save you plot
+# Contour plot for electric potential
+ax3.contour(X, Y, V_potential, colors='red', linewidths=0.5, levels=np.linspace(np.min(V_potential), np.max(V_potential), 50))
+#ax3.contour(X, Y, V_potential, colors='red', linewidths=1, levels=VpLines)
+# Color-filled contour plot
+filled_contour = ax3.contourf(X, Y, V_potential, levels=50, cmap='coolwarm')
+
+# Title and labels
+ax3.set_title("Electric Field and Equipotential Lines", fontsize=16)
+ax3.set_xlabel("x (m)", fontsize=14)
+ax3.set_ylabel("y (m)", fontsize=14)
+
+# Colorbar for the potential field
+plt.colorbar(filled_contour, ax=ax3, label="Electric Potential (V)")
+
+# Show the plot
 plt.savefig('fieldPlot2.png', dpi=300)
+
+
+
 
 
 """ 
