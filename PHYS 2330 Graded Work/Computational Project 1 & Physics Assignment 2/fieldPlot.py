423dd3bf44a66a5a09c140c126cc8c828b897716
tevin <tevinheath.beng@gmail.com>
2024-09-27 20:18:03 -0400

    Final Code for Assignment #1 & Deleted old images and added final new one

--- PHYS 2330 Graded Work/Computational Project 1 & Physics Assignment 2/fieldPlot.py
+++ PHYS 2330 Graded Work/Computational Project 1 & Physics Assignment 2/fieldPlot.py
@@ -2,13 +2,14 @@
 # Computational Exercise 1 for PHYS*2330 - Fall 2024
 # ------------------------------------------------------------------------------
 
+#Final Code: By Tevin Heath
 
 """
 # PART A
 # """
 
 """ Section 1:  Start by importing relevant libraries
-#---------------------------------------------------------------------------"""
+# #---------------------------------------------------------------------------"""
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import epsilon_0 #Found this module from scipy research
@@ -62,7 +63,7 @@ def electric_field(q, rq, X, Y):
 
     return Efield_comp_x, Efield_comp_y
 
-""" Section 3:  Main body of code 
+""" Section 3:  Main body of code
 #---------------------------------------------------------------------------"""
 # Create a 2D grid of x, y points using numpy meshgrid function
 
@@ -112,7 +113,7 @@ ax1.set_ylabel("y (m)", fontsize=14)
 
 
 # Electric potential plot (using coolwarm colour scheme)
-im1 = ax1.imshow(V_potential, cmap='coolwarm', extent=(-6, 6, -5, 5), origin="lower")
+im1 = ax1.imshow(V_potential, cmap='coolwarm', extent=(-5, 5, -5, 5), origin="lower")
 plt.colorbar(im1, ax=ax1, label="Electric Potential (V)")
 
 # Contour plot for electric potential
@@ -142,9 +143,11 @@ ax3.set_ylabel("y (m)", fontsize=14)
 # Colorbar for the potential field
 plt.colorbar(filled_contour, ax=ax3, label="Electric Potential (V)")
 
+# # Adjust layout to avoid overlap
+plt.tight_layout()
 
 # Save the figure
-plt.savefig('fieldPlot8.png', dpi=300)
+plt.savefig('fieldPlot.png', dpi=300)
 
 
 
@@ -154,18 +157,18 @@ plt.savefig('fieldPlot8.png', dpi=300)
 # Run the program once it is uncommented, then it will provide the necessary my estimation of the charges and location.
 # In PART B, I used the same structure from PART A to help with experimenting the design.
 # '''
-
-
-
+#
+#
+#
 # """ Section 1:  Start by importing relevant libraries
 # #---------------------------------------------------------------------------"""
 # import numpy as np
 # import matplotlib.pyplot as plt
 # from scipy.constants import epsilon_0
-
+#
 # """ Section 2:  Define functions for the script
 # #---------------------------------------------------------------------------"""
-
+#
 # def electric_potential(q, rq, X, Y):
 #     e_o = epsilon_0
 #     Fr_pi = (4 * np.pi)
@@ -173,115 +176,109 @@ plt.savefig('fieldPlot8.png', dpi=300)
 #     r = np.sqrt((rq[0] - X) ** 2 + (rq[1] - Y) ** 2)  # Distance between the position
 #     V = K * q / r
 #     return V
-
+#
 # def electric_field(q, rq, X, Y):
 #     e_o = epsilon_0
 #     Fr_pi = (4 * np.pi)
 #     K = 1 / (Fr_pi * e_o)
-
+#
 #     # Distance between charge and observation points
 #     r = np.sqrt((X - rq[0]) ** 2 + (Y - rq[1]) ** 2)
-
+#
 #     # Angle calculation
 #     theta = np.arctan2((Y - rq[1]), (X - rq[0]))
-
+#
 #     # Electric field components using r-squared and theta
 #     Efield_comp_x = K * q / (r ** 2) * np.cos(theta)  # x direction
 #     Efield_comp_y = K * q / (r ** 2) * np.sin(theta)  # y direction
-
+#
 #     return Efield_comp_x, Efield_comp_y
-
+#
 # """ Section 3:  Main body of code
 # #---------------------------------------------------------------------------"""
 # # Create a 2D grid of x, y points using numpy meshgrid function
-
+#
 # nx, ny = 100, 100
 # x = np.linspace(-5, 5, nx)
 # y = np.linspace(-5, 5, ny)
 # X, Y = np.meshgrid(x, y)
-
-
-
-
+#
+#
+#
+#
 # # Charges using (trial and error although I determine the charges were small)
 # q1 = -0.4
 # q2 = 0.6
 # q3 = -0.55
 # q4 = 0.6
-
+#
 # # Positions (trial and error)
 # x1, y1 = 2.2, -2
 # x2, y2 = -1.5, -3.15
 # x3, y3 = -3,3
 # x4, y4 = 0,1
-
+#
 # # Electric Potential Calculation
 # V_potential = electric_potential(q1, [x1, y1], X, Y)
 # V_potential += electric_potential(q2, [x2, y2], X, Y)
 # V_potential += electric_potential(q3, [x3, y3], X, Y)
 # V_potential += electric_potential(q4, [x4, y4], X, Y)
-
+#
 # # Electric Field Calculation (using 2D grid X and Y)
 # EField_x1, EField_y1 = electric_field(q1, [x1, y1], X, Y)
 # EField_x2, EField_y2 = electric_field(q2, [x2, y2], X, Y)
 # EField_x3, EField_y3 = electric_field(q3, [x3, y3], X, Y)
 # EField_x4, EField_y4 = electric_field(q4, [x4, y4], X, Y)
-
+#
 # # Sum the electric field components
 # Ex = EField_x1 + EField_x2 + EField_x3 + EField_x4
 # Ey = EField_y1 + EField_y2 + EField_y3 + EField_y4
 # #
 # # """ Section 4:  Plot your results as a contour plot
 # # #---------------------------------------------------------------------------"""
-
-# fig = plt.figure(figsize=(14, 14))
-
+#
+# fig = plt.figure(figsize=(20, 20))
+#
 # # Top left plot: Electric Potential
 # ax1 = fig.add_subplot(221)  # 2 rows, 2 columns, position 1
 # ax1.set_title("Electric Potential", fontsize=20)
-
+#
 # # Electric potential plot (using coolwarm colour scheme)
-# im1 = ax1.imshow(V_potential, cmap='coolwarm', extent=(-6, 6, -5, 5), origin="lower")
+# im1 = ax1.imshow(V_potential, cmap='coolwarm', extent=(-5, 5, -5, 5), origin="lower")
 # plt.colorbar(im1, ax=ax1)
-
+#
 # # Contour plot for electric potential
 # VpLines = 15.5 ** np.linspace(6, 21, 10)
 # VpLines = sorted(list(-VpLines) + list(VpLines))
 # ax1.contour(X, Y, V_potential, colors='red', linewidths=0.5, levels=VpLines)
-
-# # Top right plot: Y Meshgrid
-# ax2 = fig.add_subplot(222)  # 2 rows, 2 columns, position 2
-# ax2.set_title("Y Meshgrid", fontsize=20)
-
-# # Meshgrid plot (using coolwarm colour scheme)
-# im2 = ax2.imshow(Y, cmap='coolwarm', extent=(-6, 6, -5, 5), origin="lower")
-# plt.colorbar(im2, ax=ax2)
-
+#
+#
+#
 # # Bottom plot: Electric Field
 # ax3 = fig.add_subplot(212)  # 2 rows, 1 column, position 3 (spans both columns)
 # ax3.set_title("Electric Field and Equipotential Lines", fontsize=16)
-
+#
 # # Streamplot for the electric field (Ex, Ey)
 # ax3.streamplot(X, Y, Ex, Ey, color='blue', cmap='coolwarm', density=1.0)
-
+#
 # # Contour plot for electric potential on the electric field plot
 # ax3.contour(X, Y, V_potential, colors='red', linewidths=0.5, levels=VpLines)
-
+#
 # # Color-filled contour plot for electric potential on the electric field plot
 # filled_contour = ax3.contourf(X, Y, V_potential, levels=15, cmap='coolwarm')
-
+#
 # # Labels for the bottom plot
 # ax3.set_xlabel("x (m)", fontsize=14)
 # ax3.set_ylabel("y (m)", fontsize=14)
-
+#
 # # Colorbar for the potential field
 # plt.colorbar(filled_contour, ax=ax3, label="Electric Potential (V)")
-
-# # Adjust layout to avoid overlap
+#
+# # # Adjust layout to avoid overlap
 # plt.tight_layout()
-
+#
 # # Save the figure
-# plt.savefig('fieldPlot.png', dpi=300)
+# plt.savefig('fieldPlot10.png', dpi=300)
 
 
 
